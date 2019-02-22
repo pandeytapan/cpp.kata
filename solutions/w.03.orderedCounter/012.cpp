a3ebda4920f445bc898ce9422f8d4d1ff387bd0d
pandeytapan <pandey.tapan@hotmail.com>
2019-02-22 23:21:19 +0530

    +012 \n +test.010

--- solutions/w.03.orderedCounter/012.cpp
+++ solutions/w.03.orderedCounter/012.cpp
@@ -1,18 +1,19 @@
 #include <string.h>
+#include <ctype.h>
 #include <new>
 using namespace std;
 
 
-int * orderedCounter_010(const char * const pPhrase){
+int * orderedCounter_012(const char * const pPhrase){
 #define kALPHABET 52U
     int szAlphabets [kALPHABET] = {0};
     int nIndex;
     int nFrequency;
 
     for (nIndex = 0; pPhrase[nIndex]; ++nIndex)
-        if (pPhrase[nIndex]>='A' && pPhrase[nIndex]<='Z')
+        if (isupper(pPhrase[nIndex]))
             szAlphabets [pPhrase[nIndex] - 'A'] ++;
-        else if (pPhrase[nIndex]>='a' && pPhrase[nIndex]<='z')
+        else if (islower(pPhrase[nIndex]))
             szAlphabets [pPhrase[nIndex] - 'a' + 26] ++;
 
     nFrequency = 0;    
@@ -23,7 +24,7 @@ int * orderedCounter_010(const char * const pPhrase){
     int *szFreq = new int[nFrequency + 1]();
     nIndex = 0;
     for (nFrequency = nIndex = 0 ; nIndex < strlen(pPhrase); ++nIndex)
-        if ((pPhrase[nIndex]>='A' && pPhrase[nIndex]<='Z') || (pPhrase[nIndex]>='a' && pPhrase[nIndex]<='z'))
+        if (isalpha(pPhrase[nIndex]))
             if (szAlphabets[pPhrase[nIndex]<'a'? pPhrase[nIndex] - 'A': pPhrase[nIndex] - 'a' + 26])
             {
                 szFreq [nFrequency++] = szAlphabets[pPhrase[nIndex]<'a'? pPhrase[nIndex] - 'A': pPhrase[nIndex] - 'a' + 26];
--- solutions/w.03.orderedCounter/testorderedCounter.cpp
+++ solutions/w.03.orderedCounter/testorderedCounter.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../catch.hpp"
 #include "./010.cpp"
+#include "./012.cpp"
 
 bool assertIterableEqual (int *pIterable1, int *pIterable2, int nCount){
     while (*pIterable1++ == *pIterable2++)
@@ -8,9 +9,32 @@ bool assertIterableEqual (int *pIterable1, int *pIterable2, int nCount){
     return !nCount;
 }
 
+TEST_CASE( "Test with Zero Letters Word", "[OrderedCounter]" ) 
+{
+    const char * const pInput = "";
+    int nOutput[] = {};
+
+    REQUIRE (assertIterableEqual(orderedCounter_010(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+    REQUIRE (assertIterableEqual(orderedCounter_012(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+}
+
+
 TEST_CASE( "Test with Single Word", "[OrderedCounter]" ) 
 {
+    const char * const pInput = "mozzarella";
     int nOutput[] = {1, 1, 2, 2, 1, 1, 2};
-    REQUIRE (assertIterableEqual(orderedCounter_010("mozzarella"), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+
+    REQUIRE (assertIterableEqual(orderedCounter_010(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+    REQUIRE (assertIterableEqual(orderedCounter_012(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+   
+}
+
+TEST_CASE( "Test with Double Word", "[OrderedCounter]" ) 
+{
+    const char * const pInput = "Crime scene";
+    int nOutput[] = {1, 1, 1, 1, 3, 1, 1, 1};
+
+    REQUIRE (assertIterableEqual(orderedCounter_010(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
+    REQUIRE (assertIterableEqual(orderedCounter_012(pInput), nOutput, sizeof(nOutput)/sizeof(int)) == true);
    
 }
