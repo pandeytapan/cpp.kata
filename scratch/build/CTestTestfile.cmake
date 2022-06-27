36d58304f1f39ca8a64d788d6ab71e8054238a4e
Tapan Pandey <pandey.tapan@hotmail.com>
2022-06-27 22:04:18 +0530

    cmake updated

---
+++ scratch/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tapan/Documents/code.books/playground/cpp.code.kata/scratch
+# Build directory: /home/tapan/Documents/code.books/playground/cpp.code.kata/scratch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
