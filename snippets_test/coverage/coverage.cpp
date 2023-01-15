c21236ce5d2130f00c938cd1b5e531030179f879
Tapan Pandey <pandey.tapan@hotmail.com>
2023-01-15 22:06:01 +0530

    Changed code to read from the shared memory

--- snippets_test/coverage/coverage.cpp
+++ snippets_test/coverage/coverage.cpp
@@ -1,37 +1,58 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cctype>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 int main()
 {
-    u_int8_t    u8_vowels = 0;
-    u_int8_t    u8_consonants = 0;
-    int8_t      n8_character = 0;
-    u_int8_t    u8_others = 0;
+    u_int8_t u8_vowels = 0;
+    u_int8_t u8_consonants = 0;
+    u_int8_t u8_others = 0;
 
-    FILE *p_file = fopen("short.poem.txt", "r");
-    
-    if (p_file == NULL)
+    int32_t u32_descriptor = open("short.poem.txt", O_RDONLY | S_IRUSR);
+    if (u32_descriptor == -1)
     {
-        printf("Error opening file %s", "long.poem.txt");
+        printf("Error opening file %s", "short.poem.txt");
         return EXIT_FAILURE;
     }
-    while((n8_character = fgetc(p_file)) != EOF)
+
+    struct stat sb;
+    if (fstat(u32_descriptor, &sb) == -1)
     {
-        n8_character =  tolower(n8_character);
-        if (n8_character == 'a' || n8_character == 'e' || n8_character == 'i' || n8_character == 'o' || n8_character == 'u')
+        printf("Error opening file %s", "short.poem.txt");
+        return EXIT_FAILURE;
+    }
+
+    const u_int8_t *p_mapped_memory = (const u_int8_t *)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, u32_descriptor, 0);
+    if (p_mapped_memory == MAP_FAILED)
+    {
+        printf("Error opening file %s", "short.poem.txt");
+        return EXIT_FAILURE;
+    }
+
+    for (u_int8_t n8_idx = 0; n8_idx < sb.st_size; ++n8_idx)
+    {
+        if (tolower(p_mapped_memory[n8_idx]) == 'a' \
+        || tolower(p_mapped_memory[n8_idx]) == 'e' \
+        || tolower(p_mapped_memory[n8_idx]) == 'i' \
+        || tolower(p_mapped_memory[n8_idx]) == 'o' \
+        || tolower(p_mapped_memory[n8_idx]) == 'u')
             u8_vowels++;
-        else if (n8_character >= 'a' && n8_character <= 'z')
+        else if (tolower(p_mapped_memory[n8_idx]) >= 'a' && tolower(p_mapped_memory[n8_idx]) <= 'z')
             u8_consonants++;
         else
             u8_others++;
     }
-    
-    fclose(p_file);
+
+    munmap((void *)p_mapped_memory, sb.st_size);
+    close(u32_descriptor);
 
     printf("Vowels: %d\n", u8_vowels);
     printf("Consonants: %d\n", u8_consonants);
-    printf("Others: %d\n", u8_others);  
- 
+    printf("Others: %d\n", u8_others);
+
     return EXIT_SUCCESS;
 }
