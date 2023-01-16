ee2a422cc42ffd886513f3719bbf976cff934247
Tapan Pandey <pandey.tapan@hotmail.com>
2023-01-16 07:53:37 +0530

    Coverage code completed

--- snippets_test/coverage/coverage.cpp
+++ snippets_test/coverage/coverage.cpp
@@ -8,9 +8,6 @@
 
 int main()
 {
-    u_int8_t u8_vowels = 0;
-    u_int8_t u8_consonants = 0;
-    u_int8_t u8_others = 0;
 
     int32_t u32_descriptor = open("short.poem.txt", O_RDONLY | S_IRUSR);
     if (u32_descriptor == -1)
@@ -32,27 +29,34 @@ int main()
         printf("Error opening file %s", "short.poem.txt");
         return EXIT_FAILURE;
     }
+    pid_t process_id = fork();
+    u_int8_t u8_vowels = 0;
+    u_int8_t u8_consonants = 0;
 
-    for (u_int8_t n8_idx = 0; n8_idx < sb.st_size; ++n8_idx)
-    {
-        if (tolower(p_mapped_memory[n8_idx]) == 'a' \
-        || tolower(p_mapped_memory[n8_idx]) == 'e' \
-        || tolower(p_mapped_memory[n8_idx]) == 'i' \
-        || tolower(p_mapped_memory[n8_idx]) == 'o' \
-        || tolower(p_mapped_memory[n8_idx]) == 'u')
-            u8_vowels++;
-        else if (tolower(p_mapped_memory[n8_idx]) >= 'a' && tolower(p_mapped_memory[n8_idx]) <= 'z')
-            u8_consonants++;
-        else
-            u8_others++;
-    }
+    if (!process_id)
+        for (u_int8_t n8_idx = 0; n8_idx < sb.st_size; ++n8_idx)
+        {
+            if (tolower(p_mapped_memory[n8_idx]) == 'a' \
+            || tolower(p_mapped_memory[n8_idx]) == 'e' \
+            || tolower(p_mapped_memory[n8_idx]) == 'i' \
+            || tolower(p_mapped_memory[n8_idx]) == 'o' \
+            || tolower(p_mapped_memory[n8_idx]) == 'u')
+                u8_vowels++;
+        }
+    else
+        for (u_int8_t n8_idx = 0; n8_idx < sb.st_size; ++n8_idx)
+        {
+            if (tolower(p_mapped_memory[n8_idx]) >= 'a' && tolower(p_mapped_memory[n8_idx]) <= 'z')
+                u8_consonants++;
+        }
 
     munmap((void *)p_mapped_memory, sb.st_size);
     close(u32_descriptor);
 
-    printf("Vowels: %d\n", u8_vowels);
-    printf("Consonants: %d\n", u8_consonants);
-    printf("Others: %d\n", u8_others);
+    if(!process_id)
+        printf("Vowels: %d\n", u8_vowels);
+   else 
+        printf("Consonants: %d\n", u8_consonants);
 
     return EXIT_SUCCESS;
 }
